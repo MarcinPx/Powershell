23e91d06c1e5b8d9951ab7e2184fd0bdc65f04e0
PhotonMarcin <44867210+PhotonMarcin@users.noreply.github.com>
2018-11-15 10:46:28 +0000

    Add files via upload

---
+++ Azureexchangegettingmembersofdistributiongroup.ps1
@@ -0,0 +1,15 @@
+﻿#this script: Is getting list of members from Distribution group
+
+
+#login details for 365
+$UserCredential = Get-Credential
+
+#creating session with azure
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session -DisableNameChecking
+
+#getting memebers of distribution group and sending to file c:\test.csv (to change group put -identity "name of the group"
+Get-UnifiedGroupLinks -Identity Squad -LinkType members | select Displayname, recipienttype, primarysmtpaddress | export-csv C:\test.csv
+
+#removing session
+remove-pssession $session
